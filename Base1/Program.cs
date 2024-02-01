78b1b81b9f40693b21715ea45bb2a3bcaba371b2
nicolas carpita <carpita.nicolas@gmail.com>
2024-02-01 14:11:57 +0100

    Exercice Base1 complété

--- Base1/Program.cs
+++ Base1/Program.cs
@@ -1,1 +1,8 @@
-﻿
+﻿Console.Write("Veuillez entrer votre prénom : ");
+string prenom = Console.ReadLine();
+
+Console.Write("Veuillez entrez votre nom : ");
+string nom = Console.ReadLine();
+
+Console.WriteLine($"Vous vous appelez {prenom} {nom}");
+
