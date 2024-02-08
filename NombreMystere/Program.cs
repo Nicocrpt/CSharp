6612aba0412d17c85c825c1ee2023395a1d9bfe7
nicolas carpita <carpita.nicolas@gmail.com>
2024-02-08 12:27:01 +0100

    Debut code NombreMystere

--- NombreMystere/Program.cs
+++ NombreMystere/Program.cs
@@ -1,1 +1,18 @@
-﻿
+﻿System.Console.WriteLine("--------- Nombre Mystère --------");
+// Definir un nombre mystère entre 1 et 100 sours forme de constante entiere
+
+const int NB = 83
+
+// Demander nom utilisateur et afficher un msg de bienvenue
+
+Console.Write("Veuillez entrer votre nom : ")
+name = Console.ReadLine()
+Console.WriteLine($"Bonjour {name}")
+
+// Demander un nombre à l'utilisateur et stocker dans une variable :
+
+Console.Write($"{name}, veuillez entrer un nombre : ")
+
+int nbUser = Int.Parse(Console.ReadLine())
+
+
