6cb05ad1e6dc72daa85651bb7baeb356122d61be
nicolas carpita <carpita.nicolas@gmail.com>
2024-02-22 09:26:22 +0100

    essai multiplications

--- multiplications/Program.cs
+++ multiplications/Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Boucle for :
+
+
+// Affichage de toutes les tables :
+
+// for (int e = 1; e < 10; e++) {
+//     for (int i = 0; i < 11; i++) {
+//     System.Console.WriteLine(i*e);
+//     }
+//     System.Console.WriteLine();
+// }
+
+
+
+
+System.Console.Write("Quelle table souhaitez vous ? ");
+int tableUser = int.Parse(Console.ReadLine()) ;
+const int MAX = 10 ;
+
+System.Console.WriteLine();
+
+for (int i = 0; i <= MAX; i++) {
+    System.Console.WriteLine(i*tableUser);
+}
+
+
+
+
